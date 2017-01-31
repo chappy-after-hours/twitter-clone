8aa43c90a87380d3efd134559e0daf251b3e9978
Jeff Chapman <jeffcchapman@gmail.com>
2017-01-30 23:19:58 -0700

    into part 2

--- js/animations.js
+++ js/animations.js
@@ -2,12 +2,71 @@ $(document).ready(function(){
 
 $('#tweet-submit').hide();
 $('#char-count').hide();
+$('.tweet-actions').hide();
 
 function appendTweet(tweet) {
-  
+
+  $('#stream').prepend(
+
+    '<div class="tweet">' +
+      '<div class="content">' +
+        '<img class="avatar" src="img/alagoon.jpg">'+
+        '<strong class="fullname">Jeff Chapman</strong>'+
+        '<span class="username">@jeffcchapman</span>'+
+        '<p class="tweet-text">'+ tweet + '</p>'+
+        '<div class="tweet-actions">'+
+          '<ul>'+
+            '<li><span class="icon action-reply"></span> Reply</li>'+
+            '<li><span class="icon action-retweet"></span> Retweet</li>'+
+            '<li><span class="icon action-favorite"></span> Favorite</li>'+
+            '<li><span class="icon action-more"></span> More</li>'+
+          '</ul>'+
+        '</div>'+
+        // '<div class="stats">'+
+        //   '<div class="retweets">'+
+        //     '<p class="num-retweets">30</p>'+
+        //     '<p>RETWEETS</p>'+
+        //   '</div>'+
+        //   '<div class="favorites">'+
+        //     '<p class="num-favorites">6</p>'+
+        //     '<p>FAVORITES</p>'+
+        //   '</div>'+
+        //   '<div class="users-interact">'+
+        //     '<div>'+
+        //       '<img src="img/jennyshen.jpg" />'+
+        //       '<img src="img/damenleeturks.jpg" />'+
+        //     '</div>'+
+        //   '</div>'+
+        //   '<div class="time">'+
+        //     '1:04 PM - 19 Sep 13'+
+        //   '</div>'+
+        // '</div>'+
+        // '<div class="reply">'+
+        //   '<img class="avatar" src="img/alagoon.jpg" />'+
+        //   '<textarea class="tweet-compose" placeholder="Reply to @theonion"/></textarea>'+
+        // '</div>'+
+      '</div>'+
+    '</div>'
+
+
+
+  );
+
 }
 
-$(document).on('click','.tweet-compose',function (e) {
+$(document).on('click', '#tweet-submit', function(e) {
+  e.preventDefault();
+  var tweet = $('.tweet-compose').val();
+
+  appendTweet(tweet);
+
+  $('.tweet-compose').val('');
+  $('.tweet-compose').css("height",'2.5em');
+  $('#tweet-submit').hide();
+  $('#char-count').hide();
+})
+
+$(document).on('click', '.tweet-compose',function (e) {
   e.preventDefault();
   $('.tweet-compose').css("height",'5em');
   $('#tweet-submit').show();
@@ -28,10 +87,11 @@ $(document).on('keyup','.tweet-compose', function () {
     $('#tweet-submit').prop('disabled', false);
   }
 
-  $('tweet-submit').on('click',function(){
-
+  $('.tweet').hover(function() {
+    $('.tweet-actions').show();
+  }, function() {
+    $('.tweet-actions').hide();
   })
-
 
 })
 
